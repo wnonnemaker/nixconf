52f1aa141d43aa383e89cdbd5666211ca7d7d5c7
will <wnonnemaker@gmail.com>
2026-05-07 10:25:44 -0700

    remove hardware config

--- configuration.nix
+++ configuration.nix
@@ -128,28 +128,6 @@
 
   services.blueman.enable = true;
 
-  hardware.graphics = {
-    enable = true;
-  };
-
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-
-    modesetting.enable = true;
-
-    powerManagement.enable = false;
-
-    powerManagement.finegrained = false;
-
-    open = false;
-
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
   swapDevices = [
     {
       device = "/swapfile";
